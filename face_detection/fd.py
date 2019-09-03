bfa1becfab4c51a6131f60d26037377b927b9d83
m9 <ymgu0709@gmail.com>
2019-09-03 23:57:11 +0900

    do face-detection

--- face_detection/fd.py
+++ face_detection/fd.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('jin5.png')
+img = cv2.imread('hyoju.jpg')
 face_cascade = cv2.CascadeClassifier('haarcascade_frontface.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -11,15 +11,15 @@ if type(img) is np.ndarray:
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 print(gray.shape)
 
-faces = face_cascade.detectMultiScale(gray, 1.5, 5)
+faces = face_cascade.detectMultiScale(gray, 1.33, 5)
 
-for x,y,w,h in faces:
+for (x,y,w,h) in faces:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
     roi_gray = gray[y:y + h, x:x + w]
     roi_color = img[y:y + h, x:x + w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
-    for ex, ey, ew, eh in eyes:
+    for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-    cv2.imshow('rt', img)
-    cv2.waitKey(0)
+cv2.imshow('img', img)
+cv2.waitKey(0)
